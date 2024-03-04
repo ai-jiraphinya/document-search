21cb673675d3e6dac3f401f21f252bcd7ee99930
mint-jiraphinya <144240103+ai-jiraphinya@users.noreply.github.com>
2024-03-04 21:47:07 +0700

    Create docusaurus.config.js

---
+++ docusaurus.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Mint Jiraphinya React',
+  tagline: 'Document Search
+React By Mint Jiraphinya.',
+  url: 'https://document-search.netlify.app/',
+  baseUrl: '/',
+  trailingSlash: true,
+  favicon: 'img/favicon.png',
